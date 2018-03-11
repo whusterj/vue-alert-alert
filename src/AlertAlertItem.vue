d21029959fe2b103639ad35a319c066b5ba7cd5c
William Huster <whusterj@gmail.com>
2018-03-11 11:30:45 -0400

    Fix alert item styles (left-hand border should be colored). Bump patch version number.

--- src/AlertAlertItem.vue
+++ src/AlertAlertItem.vue
@@ -27,27 +27,26 @@ export default {
   box-shadow: 1px 1px 3px rgba(0, 0, 0, 0.6);
 
   transition: all 200ms ease-in-out;
+}
 
-  &:hover {
-    transform: scale(1.06);
-    box-shadow: 3px 3px 4px gba(0, 0, 0, 0.6);
-  }
+.aa-notification:hover {
+  transform: scale(1.06);
+  box-shadow: 3px 3px 4px rgba(0, 0, 0, 0.6);
+}
 
-  &.info {
-    border-left-color: rgb(100, 100, 200);
-  }
+.aa-notification.info {
+  border-left-color: rgb(100, 100, 200);
+}
 
-  &.success {
-    border-left-color: rgb(100, 180, 100);
-  }
+.aa-notification.success {
+  border-left-color: rgb(100, 180, 100);
+}
 
-  &.warning {
-    border-left-color: rgb(230, 215, 100);
-  }
+.aa-notification.warning {
+  border-left-color: rgb(230, 215, 100);
+}
 
-  &.error {
-    border-left-color: rgb(230, 100, 100);
-  }
-
+.aa-notification.error {
+  border-left-color: rgb(230, 100, 100);
 }
 </style>
